5e8d4c404fa48186c95ca6850b187f6e73257d5f
Alex <oleksii.pidhornyi@gmail.com>
2024-11-30 20:39:18 +0100

    finish building lists and navigation

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: String
     var name: String
     var park: String
--- Landmarks/ModelData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ModelData.swift
-//  Landmarks
-//
-//  Created by Oleksii on 28.11.2024.
-//
-
-import Foundation
