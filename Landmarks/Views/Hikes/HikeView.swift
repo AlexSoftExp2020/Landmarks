ba06b3feee35881299adaee9d9a35c26d8743b67
Alex <oleksii.pidhornyi@gmail.com>
2024-12-05 16:59:17 +0100

    animating views and transitions - animate the effects of state changes

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -11,11 +11,13 @@ struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
 
+
     var body: some View {
         VStack {
             HStack {
                 HikeGraph(hike: hike, path: \.elevation)
                     .frame(width: 50, height: 30)
+
 
                 VStack(alignment: .leading) {
                     Text(hike.name)
@@ -23,10 +25,14 @@ struct HikeView: View {
                     Text(hike.distanceText)
                 }
 
+
                 Spacer()
 
+
                 Button {
-                    showDetail.toggle()
+                    withAnimation {
+                        showDetail.toggle()
+                    }
                 } label: {
                     Label("Graph", systemImage: "chevron.right.circle")
                         .labelStyle(.iconOnly)
@@ -36,6 +42,7 @@ struct HikeView: View {
                         .padding()
                 }
             }
+
 
             if showDetail {
                 HikeDetail(hike: hike)
