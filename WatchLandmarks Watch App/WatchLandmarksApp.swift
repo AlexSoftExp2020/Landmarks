1709a124b56d18f5177258c858946d624ef9494e
Alex <oleksii.pidhornyi@gmail.com>
2024-12-11 01:01:44 +0100

    creating a watchOS app - add a watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Oleksii on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
