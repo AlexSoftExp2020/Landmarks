ea8f97a3b810991c9420334c57a78290987044ed
Alex <oleksii.pidhornyi@gmail.com>
2024-12-11 16:23:07 +0100

    creating a macOS app - update the row view

--- WatchLandmarks Watch App/LandmarkList.swift
+++ WatchLandmarks Watch App/LandmarkList.swift
@@ -1,8 +1,8 @@
 //
 //  LandmarkList.swift
-//  Landmarks
+//  WatchLandmarks Watch App
 //
-//  Created by Oleksii on 30.11.2024.
+//  Created by Oleksii on 11.12.2024.
 //
 
 import SwiftUI
@@ -10,20 +10,23 @@ import SwiftUI
 struct LandmarkList: View {
     @Environment(ModelData.self) var modelData
     @State private var showFavoritesOnly = false
-    
+
+
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-    
+
+
     var body: some View {
         NavigationSplitView {
             List {
                 Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
                 }
-                
+
+
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
@@ -34,13 +37,14 @@ struct LandmarkList: View {
             }
             .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
-            .frame(minWidth: 300)
         } detail: {
-            Text("Select a landmark")
+            Text("Select a Landmark")
         }
     }
 }
 
+
 #Preview {
     LandmarkList()
+        .environment(ModelData())
 }
