d2620602f2db9ea24c15d2456e0e705b4b13a294
Alex <oleksii.pidhornyi@gmail.com>
2024-12-12 23:04:30 +0100

    creating a macOS app - add a built-in menu command

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Oleksii on 12.12.2024.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
